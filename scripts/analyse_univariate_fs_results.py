3f31687dbe0f04db577da910997ea90b0687abe4
hmourit <hmourit@gmail.com>
2015-08-25 23:28:16 +0100

    Refactor

--- scripts/analyse_univariate_fs_results.py
+++ scripts/analyse_univariate_fs_results.py
@@ -2,11 +2,36 @@ from __future__ import division, print_function
 import json
 import sys
 from os.path import join
+from Queue import Queue
 
-from utils import extract_json_fields
 from glob import glob
 
 methods = ['anova', 'infogain_exp', 'infogain_10', 'rfe']
+
+
+def extract_json_fields(document):
+    q = Queue()
+    if isinstance(document, list):
+        for d in document:
+            q.put(('[', d))
+    elif isinstance(document, dict):
+        q.put(document)
+    else:
+        raise ValueError("Document doesn't have a valid format.")
+
+    fields = {}
+    while not q.empty():
+        prefix, d = q.get()
+        if isinstance(d, list):
+            for dd in d:
+                q.put((prefix + '[', dd))
+        elif isinstance(d, dict):
+            for key in d:
+                q.put((prefix + '.' + key, d[key]))
+        else:
+            fields.add(prefix)
+
+    return sorted(fields)
 
 
 def main():
