896b5aaee20d36f5eec93d43dae5e9db0e3bbbc4
hmourit <hmourit@gmail.com>
2015-08-25 23:24:50 +0100

    Refactor

--- scripts/analyse_univariate_fs_results.py
+++ scripts/analyse_univariate_fs_results.py
@@ -1,15 +1,23 @@
 from __future__ import division, print_function
 import json
+import sys
+from os.path import join
 
 from utils import extract_json_fields
 from glob import glob
 
 methods = {'anova', 'infogain_exp', 'infogain_10', 'rfe'}
 
-for method in methods:
-    print('### {}'.format(method))
-    files = glob('{}_*.json'.format(method))
-    document = [json.load(f) for f in files]
-    for field in extract_json_fields(document):
-        print(field)
-    print()
+
+def main():
+    path = sys.argv[0]
+    for method in methods:
+        print('### {}'.format(method))
+        files = glob(join(path, '{}_*.json'.format(method)))
+        document = [json.load(f) for f in files]
+        for field in extract_json_fields(document):
+            print(field)
+        print()
+
+if __name__ == '__main__':
+    main()
